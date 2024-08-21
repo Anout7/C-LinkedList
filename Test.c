96689af0040ba95af54087b2d61b66a7dc723d20
Anout7 <114061686+Anout7@users.noreply.github.com>
2024-08-21 16:05:35 +0800

    Update Test.c

--- Test.c
+++ Test.c
@@ -1,63 +1,63 @@
-//#include"SList.h";
-//
-//void TestList1() {
-//	SLTNode* plist = NULL;
-//
-//	SListPushBack(&plist, 1);
-//	SListPushBack(&plist, 2);
-//	SListPushBack(&plist, 3);
-//	SListPushBack(&plist, 4);
-//	SListPushBack(&plist, 5);
-//	SListPushBack(&plist, 2);
-//	SListPushBack(&plist, 6);
-//	SListPushBack(&plist, 2);
-//	SListPushBack(&plist, 7);
-//	SListPushBack(&plist, 8);
-//	SListPushBack(&plist, 2);
-//	SListPrint(plist);
-//
-//	SListPushFront(&plist, 0);
-//	SListPushFront(&plist, 1);
-//	SListPushFront(&plist, 2);
-//	SListPushFront(&plist, 3);
-//	SListPrint(plist); 
-//
-//	SListPopBack(&plist);
-//	SListPopBack(&plist);
-//	SListPrint(plist);
-//
-//	SListPopFront(&plist);
-//	SListPopFront(&plist);
-//	SListPrint(plist);
-//
-//	SLTNode* pos = SListFind(plist, 2);
-//	int i = 1;
-//	while (pos) {
-//		printf("µÚ%d¸öpos½Úµã£º%p -> %d\n", i++, pos, pos->data);
-//		pos = SListFind(pos->next, 2);
-//	}
-//
-//	//²éÕÒ->ĞŞ¸Ä
-//	pos = SListFind(plist, 6);
-//	if (pos) {
-//		pos->data = 100;
-//	}
-//	SListPrint(plist);
-//
-//	//²åÈë
-//	SLTNode* pos2= SListFind(plist, 1);
-//	if (pos2) {
-//		SListInsert(&plist, pos2, 200);
-//	}
-//	SListPrint(plist);
-//
-//	SListDestroy(plist);
-//}
-//
-//
-//
-//int main() {
-//	TestList1();
-//
-//	return 0;
-//}
+#include"SList.h";
+
+void TestList1() {
+	SLTNode* plist = NULL;
+
+	SListPushBack(&plist, 1);
+	SListPushBack(&plist, 2);
+	SListPushBack(&plist, 3);
+	SListPushBack(&plist, 4);
+	SListPushBack(&plist, 5);
+	SListPushBack(&plist, 2);
+	SListPushBack(&plist, 6);
+	SListPushBack(&plist, 2);
+	SListPushBack(&plist, 7);
+	SListPushBack(&plist, 8);
+	SListPushBack(&plist, 2);
+	SListPrint(plist);
+
+	SListPushFront(&plist, 0);
+	SListPushFront(&plist, 1);
+	SListPushFront(&plist, 2);
+	SListPushFront(&plist, 3);
+	SListPrint(plist); 
+
+	SListPopBack(&plist);
+	SListPopBack(&plist);
+	SListPrint(plist);
+
+	SListPopFront(&plist);
+	SListPopFront(&plist);
+	SListPrint(plist);
+
+	SLTNode* pos = SListFind(plist, 2);
+	int i = 1;
+	while (pos) {
+		printf("ç¬¬%dä¸ªposèŠ‚ç‚¹ï¼š%p -> %d\n", i++, pos, pos->data);
+		pos = SListFind(pos->next, 2);
+	}
+
+	//æŸ¥æ‰¾->ä¿®æ”¹
+	pos = SListFind(plist, 6);
+	if (pos) {
+		pos->data = 100;
+	}
+	SListPrint(plist);
+
+	//æ’å…¥
+	SLTNode* pos2= SListFind(plist, 1);
+	if (pos2) {
+		SListInsert(&plist, pos2, 200);
+	}
+	SListPrint(plist);
+
+	SListDestroy(plist);
+}
+
+
+
+int main() {
+	TestList1();
+
+	return 0;
+}
